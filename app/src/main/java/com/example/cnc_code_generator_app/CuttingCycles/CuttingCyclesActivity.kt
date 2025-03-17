d7dbe4ee9eaef880ff2ca937edc4a1dbf251d0a4
isarmadfarooq <i.sarmadfarooq@gmail.com>
2025-03-18 03:44:49 +0500

    Attach the cycle activities with MainActivity and do some styling and padding changes

--- app/src/main/java/com/example/cnc_code_generator_app/CuttingCycles/CuttingCyclesActivity.kt
+++ app/src/main/java/com/example/cnc_code_generator_app/CuttingCycles/CuttingCyclesActivity.kt
@@ -5,17 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.cnc_code_generator_app.R
+import com.example.cnc_code_generator_app.databinding.ActivityCuttingCyclesBinding
 
 class CuttingCyclesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCuttingCyclesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cutting_cycles)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityCuttingCyclesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         }
     }
-}
+
+
+
--- app/src/main/java/com/example/cnc_code_generator_app/ThreadingCycles/ThreadingCyclesActivity.kt
+++ app/src/main/java/com/example/cnc_code_generator_app/ThreadingCycles/ThreadingCyclesActivity.kt
@@ -5,17 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.cnc_code_generator_app.R
+import com.example.cnc_code_generator_app.databinding.ActivityThreadingCyclesBinding
 
 class ThreadingCyclesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThreadingCyclesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_threading_cycles)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityThreadingCyclesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
